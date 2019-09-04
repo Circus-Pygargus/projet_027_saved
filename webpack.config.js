89d33ddbced8d218ea661394a3fdbb2da0249ee7
Circus-Pygargus <richard.meuret@laposte.net>
2019-09-04 14:48:18 +0000

    mise en place du projet

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+var Encore = require('@symfony/webpack-encore');
+
+const CopyPlugin = require('copy-webpack-plugin');
+
+Encore
+    // directory where compiled assets will be stored
+    .setOutputPath('web/build/')
+    // public path used by the web server to access the output path
+    .setPublicPath('/build')
+    // only needed for CDN's or sub-directory deploy
+    //.setManifestKeyPrefix('build/')
+
+    /*
+     * ENTRY CONFIG
+     *
+     * Add 1 entry for each "page" of your app
+     * (including one that's included on every page - e.g. "app")
+     *
+     * Each entry will result in one JavaScript file (e.g. app.js)
+     * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
+     */
+
+    // for scss to css
+    .addStyleEntry('css/action', './assets/scss/action.scss')
+
+
+    // .addEntry('app', './assets/js/app.js')
+    //.addEntry('page1', './assets/js/page1.js')
+    //.addEntry('page2', './assets/js/page2.js')
+
+    // will require an extra script tag for runtime.js
+    // but, you probably want this, unless you're building a single-page app
+    .enableSingleRuntimeChunk()
+
+    .cleanupOutputBeforeBuild()
+    .enableSourceMaps(!Encore.isProduction())
+    // enables hashed filenames (e.g. app.abc123.css)
+    .enableVersioning(Encore.isProduction())
+
+    // uncomment if you use TypeScript
+    //.enableTypeScriptLoader()
+
+    // enables @babel/preset-env polyfills
+    .configureBabel()
+
+    // uncomment if you use Sass/SCSS files
+    .enableSassLoader()
+
+    //webpack copy plugin    
+    .addPlugin(
+        new CopyPlugin([
+          { from: './assets/img', to: 'img' }
+        ]),        
+    )
+
+    // uncomment if you're having problems with a jQuery plugin
+    //.autoProvidejQuery()
+;
+
+module.exports = Encore.getWebpackConfig();
